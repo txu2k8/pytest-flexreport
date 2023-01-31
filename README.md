8113b740c9aba1a7a389df81064ad51b8670fd24
tao.xu <t2k8>
2023-01-31 20:52:32 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # pytest-flexreport
-pytest生成html测试报告的插件，自动收集用例执行的详细日志信息，以及相关错误和输出信息
+
+### 1、pytest-flexreport介绍
+pytest-flexreport是针对pytest的生成html报告的插件，自动收集用例执行的详细日志信息，以及相关错误和输出信息。使用起来非常简单，安装好pytest-testreport之后，运行用例时加上参数即可生成报告   
+
+### 2、安装pytest-flexreport
+
+pytest-testreport是基于python3.6开发的，安装前请确认你的python版本>3.6
+
+安装命令
+
+```pip install pytest-flexreport```
+
+### 3、使用
+
+###### 注意：如果安装了pytest-html这个插件，请先卸载，不然会有冲突
+
+##### 使用案例：
+
+- ###### 命令行执行： pytest 运行测试时加上参数--report 指定报告文件名
+
+    ```shell
+    # 指定报告文件名
+    pytest --report=musen.html
+    
+    #其他配置参数
+    --title=指定报告标题
+    --tester=指定报告中的测试者
+    --desc = 指定报告中的项目描述
+    
+    # 同时使用多个参数
+    pytest --report=musen.html --title=测试报告 --tester=测试菜鸟 --desc=项目描述
+    ```
+    
+- ###### pytest.main执行
+
+    ```shell
+    import pytest
+    
+    pytest.main(['--report=musen.html',
+                 '--title=测试报告标题',
+                 '--tester=木森',
+                 '--desc=报告描述信息'])
+    ```
+
+
+
---
+++ pytestFlexReport/pytest_flexreport.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author:TXU
+@file:pytest_flexreport
+@time:2023/1/31
+@email:tao.xu2008@outlook.com
+@description: 
+"""
+import datetime
+import json
+import os
+import time
+import pytest
+from jinja2 import Environment, FileSystemLoader
+
+test_result = {
+    "title": "",
+    "tester": "",
+    "desc": "",
+    "cases": {},
+    'rerun': 0,
+    "failed": 0,
+    "passed": 0,
+    "skipped": 0,
+    "error": 0,
+    "start_time": 0,
+    "run_time": 0,
+    "begin_time": "",
+    "all": 0,
+    "testModules": set()
+}
+
+
+def pytest_make_parametrize_id(config, val, argname):
+    if isinstance(val, dict):
+        return val.get('title') or val.get('desc')
+
+
+def pytest_runtest_logreport(report):
+    report.duration = '{:.6f}'.format(report.duration)
+    test_result['testModules'].add(report.fileName)
+    if report.when == 'call':
+        test_result[report.outcome] += 1
+        test_result["cases"][report.nodeid] = report
+    elif report.outcome == 'failed':
+        report.outcome = 'error'
+        test_result['error'] += 1
+        test_result["cases"][report.nodeid] = report
+    elif report.outcome == 'skipped':
+        test_result[report.outcome] += 1
+        test_result["cases"][report.nodeid] = report
+
+
+def pytest_sessionstart(session):
+    start_ts = datetime.datetime.now()
+    test_result["start_time"] = start_ts.timestamp()
+    test_result["begin_time"] = start_ts.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def handle_history_data(report_dir, test_result):
+    """
+    处理历史数据
+    :return:
+    """
+    try:
+        with open(os.path.join(report_dir, 'history.json'), 'r', encoding='utf-8') as f:
+            history = json.load(f)
+    except:
+        history = []
+    history.append({'success': test_result['passed'],
+                    'all': test_result['all'],
+                    'fail': test_result['failed'],
+                    'skip': test_result['skipped'],
+                    'error': test_result['error'],
+                    'runtime': test_result['run_time'],
+                    'begin_time': test_result['begin_time'],
+                    'pass_rate': test_result['pass_rate'],
+                    })
+
+    with open(os.path.join(report_dir, 'history.json'), 'w', encoding='utf-8') as f:
+        json.dump(history, f, ensure_ascii=True)
+    return history
+
+
+def pytest_sessionfinish(session):
+    """在整个测试运行完成之后调用的钩子函数,可以在此处生成测试报告"""
+    report2 = session.config.getoption('--report')
+
+    if report2:
+        test_result['title'] = session.config.getoption('--title') or '测试报告'
+        test_result['tester'] = session.config.getoption('--tester') or '小测试'
+        test_result['desc'] = session.config.getoption('--desc') or '无'
+        templates_name = session.config.getoption('--template') or '1'
+        name = report2
+    else:
+        return
+
+    if not name.endswith('.html'):
+        file_name = time.strftime("%Y-%m-%d_%H_%M_%S") + name + '.html'
+    else:
+        file_name = time.strftime("%Y-%m-%d_%H_%M_%S") + name
+
+    if os.path.isdir('reports'):
+        pass
+    else:
+        os.mkdir('reports')
+    file_name = os.path.join('reports', file_name)
+    test_result["run_time"] = '{:.6f} S'.format(time.time() - test_result["start_time"])
+    test_result['all'] = len(test_result['cases'])
+    if test_result['all'] != 0:
+        test_result['pass_rate'] = '{:.2f}'.format(test_result['passed'] / test_result['all'] * 100)
+    else:
+        test_result['pass_rate'] = 0
+    # 保存历史数据
+    test_result['history'] = handle_history_data('reports', test_result)
+    # 渲染报告
+    template_path = os.path.join(os.path.dirname(__file__), './templates')
+    env = Environment(loader=FileSystemLoader(template_path))
+
+    if templates_name == '2':
+        template = env.get_template('templates2.html')
+    else:
+        template = env.get_template('templates.html')
+    report = template.render(test_result)
+    with open(file_name, 'wb') as f:
+        f.write(report.encode('utf8'))
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    fixture_extras = getattr(item.config, "extras", [])
+    plugin_extras = getattr(report, "extra", [])
+    report.extra = fixture_extras + plugin_extras
+    report.fileName = item.location[0]
+    if hasattr(item, 'callspec'):
+        report.desc = item.callspec.id or item._obj.__doc__
+    else:
+        report.desc = item._obj.__doc__
+    report.method = item.location[2].split('[')[0]
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("testreport")
+    group.addoption(
+        "--report",
+        action="store",
+        metavar="path",
+        default=None,
+        help="create html report file at given path.",
+    )
+    group.addoption(
+        "--title",
+        action="store",
+        metavar="path",
+        default=None,
+        help="pytest-testreport Generate a title of the repor",
+    )
+    group.addoption(
+        "--tester",
+        action="store",
+        metavar="path",
+        default=None,
+        help="pytest-testreport Generate a tester of the report",
+    )
+    group.addoption(
+        "--desc",
+        action="store",
+        metavar="path",
+        default=None,
+        help="pytest-testreport Generate a description of the report",
+    )
+    group.addoption(
+        "--template",
+        action="store",
+        metavar="path",
+        default=None,
+        help="pytest-testreport Generate a template of the report",
+    )
+
+
+if __name__ == '__main__':
+    pass
